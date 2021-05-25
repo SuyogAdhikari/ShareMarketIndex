311e13e54e91481e6b872489c33eb4cbabe0c47c
zer0231 <pranay_zer0@protonmail.com>
2021-05-25 23:48:27 +0545

    market ,name and average sort in ascending order

--- WebView/getData.js
+++ WebView/getData.js
@@ -19,15 +19,57 @@ firebase.initializeApp(firebaseConfig);
 
 var ref = firebase.database().ref("Companies Info");
 var counter = 1;
-
-    ref.once("value", function(snapshot) {
+var DataJSON = []
+    ref.orderByChild("CompanyName").once("value", function(snapshot) {
         snapshot.forEach(function(childSnapshot) {
          var childData =  childSnapshot.val();
-        addItemsToTable(childData.CompanyName,childData.Symbol,childData.MarketPrice,childData.EPS,childData.PE,childData.Dividend,childData.Bonus,childData.RightShare,childData.Average);
-        });
-       });
+        var CompanyName = childData.CompanyName;
+        var Symbol = childData.Symbol;
+        var MarketPrice = parseFloat(String(childData.MarketPrice).replace(/,/g, ''));
+        var EPS = childData.EPS
+        var PE = childData.PE
+        var Dividend = childData.Dividend;
+        var Bonus = childData.Bonus;
+        var RightShare = childData.RightShare;
+        var Average = parseFloat(String(childData.Average).replace(/,/g, ''));
+        if(CompanyName)
+        {
+            DataJSON.push({CompanyName,Symbol,MarketPrice,EPS,PE,Dividend,Bonus,RightShare,Average});
+            addItemsToTable(childData.CompanyName,childData.Symbol,childData.MarketPrice,childData.EPS,childData.PE,childData.Dividend,childData.Bonus,childData.RightShare,childData.Average);
+         
+        }
+    });
+       
+    });  
+  
+    function filterAverage()
+    {
+        table.innerHTML ="";
+        counter=1;
+            DataJSON.sort(function(a, b) {
+                return (a.Average) - (b.Average);
+            });
+            for(i=0;i<DataJSON.length;i++)
+            {
+                addItemsToTable(DataJSON[i].CompanyName,DataJSON[i].Symbol,DataJSON[i].MarketPrice,DataJSON[i].EPS,DataJSON[i].PE,DataJSON[i].Dividend,DataJSON[i].Bonus,DataJSON[i].RightShare,DataJSON[i].Average);
+            }
+    }
    
-
+    function filterMarketPrice()
+    {
+        table.innerHTML ="";
+        counter=1;
+        DataJSON.sort(function(a, b) {
+            return (a.MarketPrice) - (b.MarketPrice);
+        });
+        for(i=0;i<DataJSON.length;i++)
+        {
+            addItemsToTable(DataJSON[i].CompanyName,DataJSON[i].Symbol,DataJSON[i].MarketPrice,DataJSON[i].EPS,DataJSON[i].PE,DataJSON[i].Dividend,DataJSON[i].Bonus,DataJSON[i].RightShare,DataJSON[i].Average);
+        }
+    }
+    
+    
+     
        function addItemsToTable(cName,cSymbol,cMarketPrice,cEPS,cPE,cDividend,cBonus,cRightShare,cAverage)
        {
         var row = table.insertRow(-1);
@@ -55,44 +97,4 @@ var counter = 1;
        }
 
 
-// function selectAllData(){
-//     firebase.database().ref('Companies Info').once('value',
-//     function(AllRecords){
-//         AllRecords.forEach(
-//             function(CurrentRecord){
-//                 var CompanyName = CurrentRecord.val().CompanyName;
-//                 var Symbol = CurrentRecord.val().Symbol;
-//                 var MarketPrice = CurrentRecord.val().MarketPrice;
-//                 var EPS = CurrentRecord.val().EPS;
-//                 var PE = CurrentRecord.val().PE;
-//                 var Dividend = CurrentRecord.val().Dividend;
-//                 var Bonus = CurrentRecord.val().Bonus;
-//                 var RightShare = CurrentRecord.val().RightShare;
-//                 var Average = CurrentRecord.val().Average;
-//                 addItemsToTable(CompanyName,Symbol,MarketPrice,EPS,PE,Dividend,Bonus,RightShare,Average);
-//             }
-//         );
-//     });
-// }
-// window.onload = selectAllData;
-
-// -------------------------- Filling the table from data -------------------
-// var companyNumber;
-// function addItemsToTable(CompanyName,Symbol,MarketPrice,EPS,PE,Dividend,Bonus,RightShare,Average){
-//     var tbody = document.getElementById('tbody');
-//     var trow = document.createElement('tr');
-//     var td1 = document.createElement('td');    var td2 = document.createElement('td');    var td3 = document.createElement('td');    var td4 = document.createElement('td');    var td5 = document.createElement('td');    var td6 = document.createElement('td');    var td7 = document.createElement('td');    var td8 = document.createElement('td');    var td9 = document.createElement('td');  var td10 = document.createElement('td');
-//     td1.innerHTML = ++companyNumber;
-//     td2.innerHTML = CompanyName;
-//     td3.innerHTML = Symbol;
-//     td4.innerHTML = MarketPrice;
-//     td5.innerHTML = EPS;
-//     td6.innerHTML = PE;
-//     td7.innerHTML = Dividend;
-//     td8.innerHTML = Bonus;
-//     td9.innerHTML = RightShare;
-//     td10.innerHTML = Average;
-
-//     trow.appendChild(td1);    trow.appendChild(td2);    trow.appendChild(td3);    trow.appendChild(td4);    trow.appendChild(td5);    trow.appendChild(td6);    trow.appendChild(td7);    trow.appendChild(td8);    trow.appendChild(td9);    trow.appendChild(td10);
-//     trow.appendChild(trow);
-// }
+ 
