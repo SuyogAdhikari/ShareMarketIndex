f1fbb44d7e534f7d7958907644402037e91173d5
zer0231 <pranay_zer0@protonmail.com>
2021-08-09 21:47:23 +0545

    added fart sounds

--- public/js/requestData.js
+++ public/js/requestData.js
@@ -1,3 +1,10 @@
+fart = new Fart({
+default_sound: farts.toot,
+loop: false,
+volume: 100
+}); 
+
 function sendNotification(){
     alert("Sorry, This feature is under construction");
+    fart.play(farts.windy);
 }
